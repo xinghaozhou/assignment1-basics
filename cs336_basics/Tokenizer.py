f0def31ee0a12a29c2c34240aed5f44435dbf996
xinghaozhou <145933090+xinghaozhou@users.noreply.github.com>
2026-01-17 16:07:41 -0500

    Update Tokenizer using valid RankMap

--- cs336_basics/Tokenizer.py
+++ cs336_basics/Tokenizer.py
@@ -1,8 +1,3 @@
-import ast
-import regex as re
-import json
-from typing import Iterator, Iterable
-
 class Tokenizer:
     def __init__(self, 
                  vocab: dict[int, bytes], 
@@ -16,6 +11,9 @@ class Tokenizer:
         # Make a reverse look up vocab (bytes -> id) 
         self.rev_vocab = {v: k for k, v in self.vocab.items()}
 
+        # Make a rank merges for faster look up
+        self.rank_merges = {(a, b): i for i, (a, b) in enumerate(self.merges)}
+
     @classmethod
     def from_files(cls, 
                    vocab_filepath:str,
@@ -25,21 +23,32 @@ class Tokenizer:
         with open(vocab_filepath, "r", encoding="utf-8") as f:
             vocab = json.load(f)
 
+        b2u = gpt2_bytes_to_unicode()
+        rev_b2u = {v: k for k, v in b2u.items()}
+
         new_vocab: dict[tuple[bytes], int] = {}
         for k, v in vocab.items():
-            v = v.encode('utf-8')
-            new_vocab[k] = v 
+            if v in special_tokens:
+                v = v.encode('utf-8')
+            else:
+                v = bytes(rev_b2u[c] for c in v)
+
+            new_vocab[k] = v
         vocab = new_vocab
+
 
 
         with open(merges_filepath, "r", encoding="utf-8") as f:
             merges_txt = f.read().splitlines()
         
         merges = []
-        for line in merges_txt:
-            out = ast.literal_eval(line)
-            merges.append(out)
+        for i, line in enumerate(merges_txt):
+            a_str, b_str = line.rstrip("\n").split(" ", 1)
 
+            a = bytes(rev_b2u[bs] for bs in a_str)
+            b = bytes(rev_b2u[bs] for bs in b_str)
+
+            merges.append((a, b)) 
 
         tokenizer = Tokenizer(vocab, merges, special_tokens)
 
@@ -47,7 +56,7 @@ class Tokenizer:
     
 
     def encode(self, text: str) -> list[int]:
-        # First split the text by each special token
+        # Split the text by each special token
         special_token_pat = re.compile(
             "(" + "|".join(
                 re.escape(t)
@@ -55,13 +64,19 @@ class Tokenizer:
             ) + ")"
         )
 
-        matches = re.split(special_token_pat, text)
+        # First, need to check if the self.special_tokens is empty.
+        # If no special tokens, do not use special_token_pat split, otherwise, it splits on character
+        if not self.special_tokens:
+            lines = [text]
+        else:
+            matches = re.split(special_token_pat, text)
+            lines = matches # just for convenience 
 
         PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
         encoding_lst = []
         # Second, loop through each passage
-        for line in matches:
+        for line in lines:
             # If there is special tokens
             if line in self.special_tokens:
                 line = line.encode('utf-8')
@@ -79,73 +94,55 @@ class Tokenizer:
                 tok = s.encode('utf-8')
                 tok = tuple(bytes([b]) for b in tok)
 
-                for merge in self.merges:
-                    new_tup = []
+                while True:
+
+                    if len(tok) < 2:
+                        break
+
+                    # All possible pairs
+                    pairs = [(tok[i], tok[i+1]) for i in range(len(tok)-1)]
+
+                    # Find the lowest rank
+
+                    min_rank = float("inf")
+                    min_pair = None
+                    for pair in pairs:
+                        cur_rank = self.rank_merges.get(pair, float("inf"))
+
+                        if cur_rank < min_rank:
+                            min_rank = cur_rank
+                            min_pair = pair
+
+                    if min_rank == float("inf") or min_pair is None:
+                        break
+
+                    # No more merges
+                    if min_pair not in self.rank_merges:
+                        break
 
                     i = 0
+                    new_tok = []
+
+                    # Scan and merge.
                     while i < len(tok):
-                        # In pre_merge, we look at the next byte, it might be merged or not, so we use *** +1 *** rather than actual len of merge bytes
-                        if i + 1 < len(tok) and tok[i] == merge[0] and tok[i+1] == merge[1]: 
-                            new_tup.append(tok[i] + tok[i+1]) 
-                            i += 2 # Because after each merging, the elem merged together, we only need 2
+                        if i < len(tok) - 1 and (tok[i], tok[i+1]) == min_pair:
+                            new_tok.append(tok[i] + tok[i+1])
+                            i += 2
                         else:
-                            new_tup.append(tok[i])      
-                            i += 1    
+                            new_tok.append(tok[i])
+                            i += 1
 
-                    new_tup = tuple(new_tup)
-                    tok = new_tup
-
+                    tok = tuple(new_tok)
 
                 # After these, all the elem in tok will can be looked up through dict
                 for t in tok:
                     encoding_lst.append(self.rev_vocab[t])
 
-                
         return encoding_lst
 
     def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
-        PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-        special_token_pat = "|".join(re.escape(tok) for tok in self.special_tokens)
-        
-
-        for it in iterable:
-            lines = re.split(f"({special_token_pat})", it)
-
-            for line in lines:
-                # If there is special tokens, just skip
-                if line in self.special_tokens:
-                    line = line.encode('utf-8')
-                    yield self.rev_vocab[line] # using yield as "computer and deliver"
-                    continue
-
-                matches = re.finditer(PAT, line)
-
-                for match in matches:
-                    s = match.group()
-
-                    tok = s.encode('utf-8')
-                    tok = tuple(bytes([b]) for b in tok)
-
-                    for merge in self.merges:
-                        new_tup = []
-
-                        i = 0
-                        while i < len(tok):
-                            # In pre_merge, we look at the next byte, it might be merged or not, so we use *** +1 *** rather than actual len of merge bytes
-                            if i + 1 < len(tok) and tok[i] == merge[0] and tok[i+1] == merge[1]: 
-                                new_tup.append(tok[i] + tok[i+1]) 
-                                i += 2 # Because after each merging, the elem merged together, we only need 2
-                            else:
-                                new_tup.append(tok[i])      
-                                i += 1    
-
-                        new_tup = tuple(new_tup)
-                        tok = new_tup
-
-                # After these, all the elem in tok will can be looked up through dict
-                for t in tok:
-                    yield self.rev_vocab[t] # using yield as "computer and deliver"
-
+        for text in iterable:
+            yield from self.encode(text)
 
     
     def decode(self, ids: list[int]) -> str:
